421e5fba84203ba7f064565f7f991e2735da0c0b
Mwalthery <michaelwalthery@telenet.be>
2017-01-13 20:40:14 +0100

    Testing functions -> kijk boek

--- js/oefening5_functions.js
+++ js/oefening5_functions.js
@@ -1,23 +1,31 @@
+// var dag = new Date();
+// var uur = today.getHours();
+// var greeting;
+//
+// if (uur > 12){
+//
+// greeting = 'hello';
+//
+// }
+//
+// else if (uur > 18) {
+//   greeting = 'hey';
+//   }
+//
+//   else if (uur > 0) {
+//     greeting = 'hoi';
+//   }
+//
+//   else{
+//
+// greeting = 'bye';
+//
+//   }
+
 var dag = new Date();
-var uur = today.getHours();
-var greeting;
+var uur = today.getDate();
 
-if (uur > 12){
-
-greeting = 'hello';
-
+function datum (dag, uur){
+el.getElementById("Test");
+  el.textContent( dag );
 }
-
-else if (uur > 18) {
-  greeting = 'hey';
-  }
-
-  else if (uur > 0) {
-    greeting = 'hoi';
-  }
-
-  else{
-
-greeting = 'bye';
-
-  }
